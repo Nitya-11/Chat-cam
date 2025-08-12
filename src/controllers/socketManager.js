251d82dd99513ee9b42ea01304d3446d62689e33
nitya singh <nityasingh559@gmail.com>
2025-08-12 22:37:56 +0530

    add code

---
+++ src/controllers/socketManager.js
@@ -0,0 +1,45 @@
+import {Server} from "socket.io";
+
+let connections = {}
+let messages = {}
+let timeOnline = {}
+
+export const connectToSocket = (server) => {
+    const io = new Server(server);
+
+    io.on("connection",(socket) => {
+        socket.on("join-call",(path) => {
+           
+            if(connections[path] === undefined) {
+                connections[path] = []
+            }
+             connections[path].push(socket.id)
+
+             timeOnline[socket.id] = new Date();
+
+            //  connections[path].forEach(elem => {
+            //       io.to(elem);
+            //  })
+
+             for(let a = 0; a < connections[path].length; i++) {
+                io.to(connections[path[a]])
+             }
+        })
+        
+        socket.on("signal",(toId,message) =>{
+            io.to(toId).emit("signal",socket.id,message);
+        })
+
+        socket.on("chat-message",(data,sender) => {
+
+        })
+
+        socket.on("disconnect",() => {
+            
+        })
+
+    })
+
+    return io;
+}
+
